f1bdeb52e189ab5beb21180481025e3c9ad6336e
Bilal <bilals@hotmail.se>
2023-02-12 18:05:11 +0100

    textfiler

--- TextFiler.cs
+++ TextFiler.cs
@@ -11,7 +11,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace GruppUppgift_Väderdata
 {
-    internal class TextFiler
+    internal class TextFiler //s
     {
         static string path = "../../../Textfiler/";
         static string pattern = @"(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2}),(\w+),(-?\d+\.\d+),(\d+)";
